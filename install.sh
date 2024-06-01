833b3efcc3dc5b0a17ad9686edc5bd66e8de3384
Yacine Floret <yacine.floret@protonmail.com>
2024-06-01 11:54:58 +0200

    Removing unecessary line

--- install.sh
+++ install.sh
@@ -22,7 +22,6 @@ done
 
 echo "Tous les packages ont été installés avec succès."
 chsh -s /usr/bin/zsh
-/usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sS https://starship.rs/install.sh | sh
 cp .fzf_env "${HOME}/.fzf_env"
